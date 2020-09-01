b23cdb1f7e1dd0cf272257d40954afc6b4c4efc5
Ajay Raj <ajayraj003016@gmail.com>
2020-09-01 14:40:07 +0530

    create routing for laptop and products

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const fs  = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`);
 const  laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, 'Content-type: text/html');
-    res.end('This is the response!');
+    const pathName = url.parse(req.url, true).pathname;
+    const query = url.parse(req.url, true).query;
+
+    if(pathName === '/products' || pathName === '/') {
+        res.writeHead(200, 'Content-type: text/html');
+        res.end('This is the products page!');
+    } 
+
+    else if(pathName === '/laptops' && query.id < laptopData.length) {
+        res.writeHead(200, 'Content-type: text/html');
+        res.end(`This is the laptop page for laptop ${query.id}!`);
+    }
+    
+    else {
+        res.writeHead(404, 'Content-type: text/html');
+        res.end('URL was not found on the server');
+    }
 });
 
 server.listen(1337, '127.0.0.1', () => {
